fc21c7c949b5a9dd613b3ecf90259f5a12e48625
Biraj Ghosh <birajghosh6@gmail.com>
2020-09-06 16:13:27 +0530

    Changed App.js to a class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
